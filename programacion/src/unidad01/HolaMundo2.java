908a9823c376e8264e9cc5dc0fe444fc093d74bc
dquipar304 <dquipar304@g.educaand.es>
2024-09-26 19:07:26 +0200

    esto es para probar si no se borran los otros archivos

--- programacion/src/unidad01/HolaMundo2.java
+++ programacion/src/unidad01/HolaMundo2.java
@@ -8,11 +8,30 @@ public class HolaMundo2 {
 		// SI INTRODUCE NOMBRE LE DEVOLVEMOS HOLA + SU NOMBRE, SI NO, LE DECIMOS HOLA AMIGO.
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Introduce tu nombre! (opcional): ");
-		String nombre = scan.nextLine(); // Cuando el usuario no introduce ningun nombre se guada una cadena vacia. ""
+		String nombre = scan.nextLine(); 
+		// Cuando el usuario no introduce ningun nombre se guada una cadena vacia. ""
 		// ESTA ES LA FORMA DE COMPRAR CADENAS. NO CON ==.
-		nombre = "".equals(nombre) ? "amigo" : nombre;
+	
+		// VERSIÓN SIN IF:::::
+		/* 	nombre = "".equals(nombre) ? "amigo" : nombre;
 		System.out.println("Hola " + nombre + "!!");
+		*/
+		
+		
+		
+		// version if compuesto
+		if (!"".equals(nombre)) {
+			// si se cumple la condición
+			System.out.println("Hola " + nombre + "!!");
+		} else { 
+			// si no se cumple la condición
+			System.out.println("Hola amigo!");
+		}
+		
+		
 		scan.close();
+		
+		
 		
 
 	
