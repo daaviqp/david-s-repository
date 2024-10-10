4c978b87ce5373c1d8945102263c381cdf4f5c2f
David Quirós <dquipar304@g.educaand.es>
2024-10-10 20:38:36 +0200

    tarea2

---
+++ com.davidQuiros.tarea2/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module com.davidQuiros.tarea2 {
+}
---
+++ programacion/src/unidad02/InsertarContraseña.java
@@ -0,0 +1,10 @@
+package unidad02;
+
+public class InsertarContraseña {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
