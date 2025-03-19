1cf6b527a8103bdbae223a6a44f4cc629f8c2a27
David Quirós <dquipar304@g.educaand.es>
2025-03-19 20:17:56 +0100

    Atropodos

---
+++ Programame2025/src/Programame2025/Atropodos293.java
@@ -0,0 +1,41 @@
+package Programame2025;
+
+//Esquema de la entrada: número de casos
+import java.util.*;
+
+public class Atropodos293 {
+
+static Scanner in;
+
+public static void casoDePrueba() {
+
+ // TU CÓDIGO AQUÍ.
+ // Procesa un único caso leyendo con
+ // in.next*()
+	
+	int insectos = in.nextInt();
+	int aracnidos = in.nextInt();
+	int crustaceos = in.nextInt();
+	int escalopendras = in.nextInt();
+	int numeroAnillosEscalopendras = in.nextInt();
+	
+	insectos *= 6;
+	aracnidos *= 8;
+	crustaceos *= 10;
+	escalopendras *= numeroAnillosEscalopendras * 2;
+	int totalPatas = insectos + aracnidos + crustaceos + escalopendras;
+	System.out.println(totalPatas);
+
+} // casoDePrueba
+
+public static void main(String[] args) {
+
+ in = new java.util.Scanner(System.in);
+
+ int numCasos = in.nextInt();
+ for (int i = 0; i < numCasos; i++)
+   casoDePrueba();
+
+} // main
+
+} // class Solution
